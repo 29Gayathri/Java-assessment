2b1a4a1c413c34a7c549af7e6323c90e1a4e9a7d
29Gayathri <101647553+29Gayathri@users.noreply.github.com>
2022-03-23 10:39:33 +0530

    Add files via upload

---
+++ DisplayName.java
@@ -0,0 +1,13 @@
+class Name {
+	public void display() {
+		System.out.println("Gayathri S");
+	}
+}
+
+public class DisplayName {
+	public static void main(String[] args) {
+		 Name myname = new Name();
+				 myname.display();
+	 }
+
+}
